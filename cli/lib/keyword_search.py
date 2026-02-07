98b9a098e6ce12703ea33ad0ff6b6a98ac32ed0a
rav666 <harshitagaurav2609@gmail.com>
2026-02-07 21:52:24 +0530

    Tokenizing

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -7,12 +7,28 @@ def clean_text(text):
     text = text.translate(str.maketrans('', '', string.punctuation))
 
     return text
+
+
+def tokenize_text(text):
+    text = clean_text(text)
+    tokens = [i for i in text.split() if i]
+    return tokens
+
+def is_matching(query_toks, movie_toks):
+
+    for query_tok in query_toks:
+        for movie_tok in movie_toks:
+            if query_tok in movie_tok:
+                return True
+    return False
+
 def search_movies(query, n_results):
     movies = load_movies()
     result = []
-    query = clean_text(query)
+    query_tokens = tokenize_text(query)
     for movie in movies:
-        if query.lower() in clean_text(movie['title']):
+        movie_tokens = tokenize_text(movie['title'])
+        if is_matching(query_tokens, movie_tokens):
             result.append(movie)
         if len(result) == n_results:
             break
