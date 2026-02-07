afd2f5dfb2c7d7205a0a380fe1dd924e9f6e211e
rav666 <harshitagaurav2609@gmail.com>
2026-02-07 20:43:18 +0530

    Added punctuation rectification

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,9 +1,18 @@
 from lib.search_utils import load_movies
+import string
+
+def clean_text(text):
+    text= text.lower()
+
+    text = text.translate(str.maketrans('', '', string.punctuation))
+
+    return text
 def search_movies(query, n_results):
     movies = load_movies()
     result = []
+    query = clean_text(query)
     for movie in movies:
-        if query.lower() in movie['title'].lower():
+        if query.lower() in clean_text(movie['title']):
             result.append(movie)
         if len(result) == n_results:
             break
