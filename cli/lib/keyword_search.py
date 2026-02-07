ff24e9ee63365b9de47d911c9ebdc31463208903
rav666 <harshitagaurav2609@gmail.com>
2026-02-07 20:32:21 +0530

    Case Insensitivity

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -3,7 +3,7 @@ def search_movies(query, n_results):
     movies = load_movies()
     result = []
     for movie in movies:
-        if query in movie['title']:
+        if query.lower() in movie['title'].lower():
             result.append(movie)
         if len(result) == n_results:
             break
