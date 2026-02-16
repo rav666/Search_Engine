f0c4b259d0674ecbc496ffacc6edb63c604847fd
rav666 <harshitagaurav2609@gmail.com>
2026-02-16 22:09:13 +0530

    normalized scores of sorts

---
+++ cli/hybrid_search_cli.py
@@ -0,0 +1,24 @@
+import argparse
+
+from lib.hybrid_search import normalize_scores
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Hybrid Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    norm_parser = subparsers.add_parser(name="normalized", help="Available commands")
+    norm_parser.add_argument(dest='scores', type=float, nargs='+', help="Score to normalize")
+
+    args = parser.parse_args()
+
+    match args.command:
+        case 'normalized':
+            norm_scores = normalize_scores(args.scores)
+            for norm_score in norm_scores:
+                print(f"{norm_score:.4f}")
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
